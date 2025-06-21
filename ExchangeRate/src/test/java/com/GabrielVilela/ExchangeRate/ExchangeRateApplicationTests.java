84c8c204cf295de30e8466055669516740f0dc91
GabrielbVilela <Gabrielbvilelal@gmail.com>
2025-06-21 16:32:48 -0300

    Init project
    Cria um novo projeto com spring. Esse novo projeto será baseado em um repositório antigo chamado AluraExchangeChallenge

---
+++ ExchangeRate/src/test/java/com/GabrielVilela/ExchangeRate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielVilela.ExchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
